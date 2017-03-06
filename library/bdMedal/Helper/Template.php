f90511302fbcc4d310521f32990e07d22f2dd795
Dao Hoang Son <daohoangson@gmail.com>
2017-03-06 11:26:58 +0700

    Fix template error in member_view

--- library/bdMedal/Helper/Template.php
+++ library/bdMedal/Helper/Template.php
@@ -2,27 +2,35 @@
 
 class bdMedal_Helper_Template
 {
-    public static function renderCachedMedals($template, array $params)
+    public static function getCachedMedalsCount($default, array $params)
     {
-        if (!isset($params['user'])) {
-            return '';
-        }
-        $user = $params['user'];
-        if (empty($user['user_id'])
-            || !isset($user['xf_bdmedal_awarded_cached'])
-        ) {
-            return '';
+        try {
+            list(, $medals) = self::_getCachedMedals($params);
+        } catch (XenForo_Exception $e) {
+            if (XenForo_Application::debugMode()) {
+                XenForo_Error::logException($e, false);
+            }
+
+            return $default;
         }
 
-        if (!is_array($user['xf_bdmedal_awarded_cached'])) {
-            $user['xf_bdmedal_awarded_cached'] = @unserialize($user['xf_bdmedal_awarded_cached']);
-        }
-        if (empty($user['xf_bdmedal_awarded_cached'])) {
+        return count($medals);
+    }
+
+    public static function renderCachedMedals($template, array $params)
+    {
+        try {
+            list($user, $medals) = self::_getCachedMedals($params);
+        } catch (XenForo_Exception $e) {
+            if (XenForo_Application::debugMode()) {
+                XenForo_Error::logException($e, false);
+            }
+
             return '';
         }
 
         $awardedModel = self::_getAwardedModel();
-        $awardedMedals = $awardedModel->applyOrganizedOrder($user['xf_bdmedal_awarded_cached']);
+        $awardedMedals = $awardedModel->applyOrganizedOrder($medals);
 
         $output = array();
         foreach ($awardedMedals as $awardedMedal) {
@@ -57,5 +65,33 @@ class bdMedal_Helper_Template
         }
 
         return $model;
+    }
+
+    /**
+     * @param array $params
+     * @return array
+     * @throws XenForo_Exception
+     */
+    protected static function _getCachedMedals(array $params)
+    {
+        if (!isset($params['user'])) {
+            throw new XenForo_Exception('No user');
+        }
+        $user = $params['user'];
+        if (empty($user['user_id'])
+            || !isset($user['xf_bdmedal_awarded_cached'])
+        ) {
+            throw new XenForo_Exception('No data');
+        }
+
+        $medals = $user['xf_bdmedal_awarded_cached'];
+        if (!is_array($medals)) {
+            $medals = @unserialize($medals);
+        }
+        if (!is_array($medals)) {
+            throw new XenForo_Exception('Invalid data');
+        }
+
+        return array($user, $medals);
     }
 }
