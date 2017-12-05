xfrocks/bdMedal

commit 7ae135ab3f0425c6a58383643592dfe7af2b0d3e

Remove usage of each()

library/bdMedal/DataWriter/Medal.php
@@ -55,7 +55,8 @@ class bdMedal_DataWriter_Medal extends XenForo_DataWriter
         reset($imageSizes);
 
         if ($uploadSizeCode == 'l') {
-            while (list($sizeCode, $maxDimensions) = each($imageSizes)) {
+            foreach($imageSizes as $imageSize) {
+                list($sizeCode, $maxDimensions) = $imageSize;
                 if (isset($outputFiles[$sizeCode])) {
                     continue;
                 }

commit ec04f78023b98c5c631275553a00e3b4fc8a81bc

Fix bug processing medal images caused by 7ae135a

library/bdMedal/DataWriter/Medal.php
@@ -52,11 +52,9 @@ class bdMedal_DataWriter_Medal extends XenForo_DataWriter
         $imageType = $upload->getImageInfoField('type');
         $outputType = $imageType;
         $imageSizes = self::getImageSizes();
-        reset($imageSizes);
 
         if ($uploadSizeCode == 'l') {
-            foreach($imageSizes as $imageSize) {
-                list($sizeCode, $maxDimensions) = $imageSize;
+            foreach ($imageSizes as $sizeCode => $maxDimensions) {
                 if (isset($outputFiles[$sizeCode])) {
                     continue;
                 }